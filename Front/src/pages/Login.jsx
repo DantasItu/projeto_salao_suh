02644871043e90e1b1fb88ac550ad603d48f2476
unknown <michael.of.douglasaraujodantas5@gmail.com>
2025-06-01 13:07:17 -0300

    finalizado o css do login

--- Front/src/pages/Login.jsx
+++ Front/src/pages/Login.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { loginApi } from "../Api/authServices.js";
 import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
+import logo from "../data/icon/logo redondo.png";
+import "../data/styles/login.css";
 
 const Login = () => {
   // funções de status
@@ -28,29 +30,40 @@ const Login = () => {
           : "/"
       );
     } catch (err) {
-      alert("login inválido");
-      console.error(err);
+      alert(`Login inválido!`);
     }
   };
 
   return (
     <>
-      <div className="login">
-        <form className="caixa_login" onSubmit={statusLogin}>
-          <input
-            type="text"
-            placeholder="Digite seu email ou Seu nome..."
-            value={login}
-            onChange={(e) => setlogin(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Digite sua senha..."
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button type="submit"> Entrar </button>
-        </form>
+      <div className="body_login">
+        <div className="login">
+          <form className="caixa_login" onSubmit={statusLogin}>
+            <img className="login_logo" src={logo} />
+            <div className="login_anuciado">
+              <h2>Bem Vindo!</h2>
+              <p>Faça seu Login.</p>
+            </div>
+            <input
+              className="box_login_imput"
+              type="text"
+              placeholder="Digite seu email ou Seu nome..."
+              value={login}
+              onChange={(e) => setlogin(e.target.value)}
+            />
+            <input
+              className="box_login_imput"
+              type="password"
+              placeholder="Digite sua senha..."
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button className="box_login_button" type="submit">
+              {" "}
+              Entrar{" "}
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
